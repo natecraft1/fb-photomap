4932a5c85d334f16a5076f9d9e04eb28af8f5879
nate dawg <nglass1988@gmail.com>
2014-03-17 15:41:21 -0700

    RoRd fbphotomap

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FbPhotomap::Application.config.secret_key_base = 'bf80a3e56809a350fc7b09394ca2791455718311dad8afd3424e1e280ef81d5baf6e639f5c66ee5b655dddf6000d16e5e993dedbf2cdf351897d2caccfc85b27'
